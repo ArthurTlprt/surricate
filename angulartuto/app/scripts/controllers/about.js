b583567b630b895da0b09bb7c821672f7107ba88
ArthurT <arthur.talpaert@isen-lille.fr>
2016-03-08 18:18:21 +0100

    init express generate skeleton

---
+++ angulartuto/app/scripts/controllers/about.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name magoApp.controller:AboutCtrl
+ * @description
+ * # AboutCtrl
+ * Controller of the magoApp
+ */
+angular.module('magoApp')
+  .controller('AboutCtrl', function () {
+    this.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
