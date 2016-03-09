d17b56009ea8b7fe347a63e3e1eba1f793027fb3
ArthurT <arthur.talpaert@isen-lille.fr>
2016-03-09 12:48:15 +0100

    integration du template

--- surricate/routes/index.js
+++ surricate/routes/index.js
@@ -3,7 +3,15 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+  res.render('index',
+  {title: 'Surricates illuminés',
+  nav: {
+      li1: 'La troupe',
+      li2: 'Actualité',
+      li3: 'Parcours',
+      li4: 'Les comédiens',
+      li5: 'Nous contacter'
+  }});
 });
 
 module.exports = router;
